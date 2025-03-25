5c665be80da09bf5b95f428f5db5034b0f992fc9
Viacheslav-ua <atph@ukr.net>
2025-03-25 10:50:03 +0000

    geting keys

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use solana_sdk::signature::{Keypair, Signer};
+
+
 fn main() {
-    println!("Starting");
+    let keypair: Keypair = Keypair::new();
+    
+    let private_key: [u8; 64] = keypair.to_bytes();
+    let public_key: solana_sdk::pubkey::Pubkey = keypair.pubkey();
+
+    println!("Public Key: {}", public_key);
+    println!("Private Key: {:?}", private_key);
 }
