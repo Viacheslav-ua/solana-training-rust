356da250f3f6f01bb6f6284e76e71acff366c2cc
Viacheslav-ua <atph@ukr.net>
2025-03-25 13:27:14 +0000

    saving keys

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use solana_sdk::signature::{Keypair, Signer};
-
+use std::fs;
 
 fn main() {
     let keypair: Keypair = Keypair::new();
     
-    let private_key: [u8; 64] = keypair.to_bytes();
-    let public_key: solana_sdk::pubkey::Pubkey = keypair.pubkey();
+    let private_key: String = bs58::encode(keypair.to_bytes()).into_string();
+    let public_key: String = keypair.pubkey().to_string();
 
     println!("Public Key: {}", public_key);
-    println!("Private Key: {:?}", private_key);
+    println!("Private Key: {}", private_key);
+
+    let env_content: String = format!("PRIVATE_KEY={}\nPUBLIC_KEY={}\n", private_key, public_key);
+    fs::write("./.env", env_content).expect("Failed to write to .env file");
+
+    println!("✅ The keys saved to .env file");
 }
