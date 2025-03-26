c267c5a083bb4971e39f8bdee7399a8239b06de5
Viacheslav-ua <atph@ukr.net>
2025-03-26 16:10:13 +0000

    added read_keys function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use solana_sdk::signature::{Keypair, Signer};
+use dotenv::dotenv;
+use dotenv::from_filename;
+use std::env;
 use std::io;
 use std::fs;
 
 fn main() {
+    dotenv().ok();
     loop {
         let mut action: String = String::new();
 
@@ -23,7 +27,7 @@ fn main() {
 
         match &command {
             1 => { generate_keys(); },
-            2 => println!("Read keys"),
+            2 => { read_keys(); },
             0 => { break; },
             _ => println!("\x1b[31mInvalid command\x1b[0m\nUse only \x1b[32m0\x1b[0m, \x1b[32m1\x1b[0m or \x1b[32m2\x1b[0m"),
         }
@@ -45,4 +49,41 @@ fn generate_keys() {
     fs::write("./.env", env_content).expect("Failed to write to .env file");
 
     println!("✅ The keys saved to .env file");
+    reload_env()
+}
+
+fn read_keys() {
+       
+    
+    let private_key = match env::var("PRIVATE_KEY") {
+        Ok(key) => key,
+        Err(_) => {
+            eprintln!("❌ Error: \x1b[91mPRIVATE_KEY value not found in the env\x1b[0m");
+            return;
+        }
+    };
+
+    let _private_key_bytes = match bs58::decode(&private_key).into_vec() {
+        Ok(bytes) if bytes.len() == 64 => bytes,
+        _ => {
+            eprintln!("❌ Error: \x1b[91mInvalid private key format\x1b[0m");
+            return;
+        }
+    };
+    
+    let keypair: Keypair = match Keypair::from_bytes(&_private_key_bytes) {
+        Ok(kp) => kp,
+        Err(_) => {
+            eprintln!("❌ Error: \x1b[91mFailed to create Keypair\x1b[0m");
+            return;
+        }
+    };
+
+    println!("✅ \x1b[95mPublic key:\x1b[0m \x1b[4;34m{}\x1b[0m", keypair.pubkey());
+}
+
+
+fn reload_env() {
+    env::vars().for_each(|(key, _)| unsafe { env::remove_var(key) });
+    from_filename(".env").ok();
 }
